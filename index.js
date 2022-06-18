25287db9c9742cb32f88894d2b18e648b2d7a58d
Laura Gonzalez <lg458@cornell.edu>
2022-06-18 12:40:51 -0700

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+//removing DOM node main#main
+const element = document.getElementById("main") 
+element.remove(); 
+
+//creating new header
+const newHeader = document.createElement("h1"); 
+newHeader.setAttribute('id', 'victory'); 
+
+//adds text to newHeader node 
+newHeader.textContent = "Laura is the champion";
